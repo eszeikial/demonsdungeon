db0a2630c7e1f7c5a4690c258b432bcb7b4cf010
eszeikial <eszeikial@gmail.com>
2013-11-04 17:21:52 -0500

    Maze Scripts - Update
    wall pieces now have more information about themselves.
    walls update their own location.
    Randomly generating walls and moving walls randomly.

--- UnityFiles/Assets/Scripts/WallManager.cs
+++ UnityFiles/Assets/Scripts/WallManager.cs
@@ -16,13 +16,20 @@ public class WallManager: MonoBehaviour {
 	GameObject[][] ZWallArray;
 	float[][] XWallState; //value is 0.0 - 1.0
 	float[][] ZWallState; //0 being down, 1 being entirely raised.
- 	
 	int numPieces;
+	GameObject parent;
 	
+	//randomizer
+	System.Random rng;
 	
-	// Use this for initialization
+	/// <summary>
+	/// Creates the WallManager instance.
+	/// The maze objects are all created and added to the heiarchy neatly.
+	/// The walls are generated with a yLoc value of either 0 or 1, up or down.
+	/// </summary>
 	void Start () {
-		
+		rng = new System.Random();
+		parent = GameObject.Find("WallManagerGO");
 		numPieces = (int)(mapSize/scale) - 1;
 		
 		//Creating pillars
@@ -33,7 +40,9 @@ public class WallManager: MonoBehaviour {
 			
 			for(int j = 0; j < numPieces; j++)
 			{
-				pillarArray[i][j]  = (GameObject)Object.Instantiate(Pillar, new Vector3(i*scale - (mapSize/2) + scale , 1, j*scale -(mapSize/2) + scale),Quaternion.identity);
+				pillarArray[i][j]  = (GameObject)Object.Instantiate
+					(Pillar, new Vector3(i*scale - (mapSize/2) + scale , 1, j*scale -(mapSize/2) + scale),Quaternion.identity);
+				pillarArray[i][j].transform.parent = parent.transform; //sets the GO neatly in heiarchy. 
 			}
 		}
 		
@@ -47,10 +56,16 @@ public class WallManager: MonoBehaviour {
 			
 			for(int j = 0; j < numPieces; j++)
 			{
-				XWallArray[i][j]  = (GameObject)Object.Instantiate(XWall, new Vector3(i*scale - (mapSize/2) + .5f * scale , 1, j*scale -(mapSize/2) + scale),Quaternion.identity);
+				int rnd = rng.Next()%2; // Randomly decide if wall is raised or not.
+				
+				XWallArray[i][j]  = (GameObject)Object.Instantiate
+					(XWall, new Vector3(i*scale - (mapSize/2) + .5f * scale , 1, j*scale -(mapSize/2) + scale),Quaternion.identity);
 				XWallState[i][j] = 1.0f;
 				
-				XWallArray[i][j].GetComponent<WallScript>().Setup(i,j,1.0f,-.75f); //gives the wall info about itself, like its location, max/min height.
+				XWallArray[i][j].transform.parent = parent.transform; //sets the GO neatly in heiarchy. 
+				XWallArray[i][j].GetComponent<WallScript>().Setup(i,j,.75f,-1.75f); //gives the wall info about itself, like its location, max/min height.
+				XWallArray[i][j].GetComponent<WallScript>().SetParent(XWallArray[i][j]); // lets the object know about itself.
+				XWallArray[i][j].GetComponent<WallScript>().SetHeight((float)rnd); // sets height.
 			}
 		}
 		
@@ -63,10 +78,16 @@ public class WallManager: MonoBehaviour {
 			ZWallState[i] = new float[numPieces+1];
 			for(int j = 0; j < numPieces+1; j++)
 			{
-				ZWallArray[i][j] = (GameObject)Object.Instantiate(ZWall, new Vector3(i*scale - (mapSize/2) + scale, 1, j*scale -(mapSize/2) + .5f * scale ),Quaternion.identity);
+				int rnd = rng.Next()%2; // Randomly decide if wall is raised or not.
+				
+				ZWallArray[i][j] = (GameObject)Object.Instantiate
+					(ZWall, new Vector3(i*scale - (mapSize/2) + scale, 1, j*scale -(mapSize/2) + .5f * scale ),Quaternion.identity);
 				ZWallState[i][j] = 1.0f; 
 				
-				ZWallArray[i][j].GetComponent<WallScript>().Setup(i,j,1.0f,-.75f); //gives the wall info about itself, like its location, max/min height.
+				ZWallArray[i][j].transform.parent = parent.transform; //sets the GO neatly in heiarchy. 
+				ZWallArray[i][j].GetComponent<WallScript>().Setup(i,j,.75f,-1.75f); //gives the wall info about itself, like its location, max/min height.
+				ZWallArray[i][j].GetComponent<WallScript>().SetParent(ZWallArray[i][j]); // lets the object know about itself.
+				ZWallArray[i][j].GetComponent<WallScript>().SetHeight((float)rnd); // sets height.
 			}
 		}
 		
@@ -75,5 +96,38 @@ public class WallManager: MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+		//Every 5 frames randomly move a wall.
+		if(Time.frameCount %5 == 0)
+		{
+			randomlyMoveWall();
+		}
+	}
+	
+	private void randomlyMoveWall()
+	{
+		//Pick a random wall and move it
+		int wallNum = rng.Next()%380;
+		int wallX = wallNum/20; // number between 0-18
+		int wallY = wallNum%20; // number between 0-19
+		WallScript ws;
+		
+		
+		if(rng.Next()%2 == 0)
+		{
+			//Use xWalls	
+			ws = XWallArray[wallY][wallX].GetComponent<WallScript>();
+		}else{
+			//Use zWalls
+			ws = ZWallArray[wallX][wallY].GetComponent<WallScript>();
+		}
+		
+		if(ws.GetHeight() == 1.0f)
+		{
+			ws.SetHeight(0.0f);	
+		}
+		else if(ws.GetHeight() == 0.0f)
+		{
+			ws.SetHeight(1.0f);	
+		}
 	}
 }
--- UnityFiles/Assets/Scripts/WallScript.cs
+++ UnityFiles/Assets/Scripts/WallScript.cs
@@ -9,14 +9,20 @@ public class WallScript : MonoBehaviour {
 	float maxHeight;
 	float minHeight;
 	
+	//self
+	GameObject thisGO;
+	Transform trans;
+	
 	//Height
 	float yLoc; // must be 0.0-1.0.
+	float currYLoc;
 	
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+	//Sets the initial values, is called right after being created by WallManager
 	public void Setup(int r, int c, float maxHeight, float minHeight)
 	{
 		row = r;
@@ -25,8 +31,46 @@ public class WallScript : MonoBehaviour {
 		this.minHeight = minHeight;
 	}
 	
+	//Sends the parent down as well.
+	public void SetParent(GameObject go)
+	{
+		thisGO = go;
+		trans = go.transform;
+	}
+	
+	
+	public void SetHeight(float val)
+	{
+		if(val >= 0.0f && val <= 1.0f)
+			yLoc = val;
+	}
+	
+	public float GetHeight()
+	{
+		return yLoc;	
+	}
+	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		if(Mathf.Abs (currYLoc-yLoc) > .05f)
+		{
+			if (currYLoc > yLoc)
+			{
+				currYLoc -= .05f;
+			}else{
+				currYLoc += .05f;
+			}
+		}
+		
+		//Calculates the height based on currYLoc.
+		float heightMod = maxHeight - minHeight;
+		heightMod = heightMod*currYLoc;
+		
+		float yPos = heightMod + minHeight;
+		float xPos = trans.position.x;
+		float zPos = trans.position.z;
+		
+		trans.position = new Vector3(xPos,yPos,zPos);
 	}
 }
