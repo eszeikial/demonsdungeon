fc9cc137063a8557c4ee5e7ece88f0f7084d2a8c
cskatrud <cjs1229@rit.edu>
2013-12-10 15:50:28 -0500

    detecting collision between mage and skull

---
+++ UnityFiles/Assets/Scripts/PlayerCollisions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollisions : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider c)
+	{
+		Debug.Log("Player collided with" + c.gameObject.tag);
+		if(c.gameObject.tag == "skull")
+		{
+			Debug.Log("Mage dead");
+			
+			Network.CloseConnection(Network.player, false);
+		}
+	}
+}
